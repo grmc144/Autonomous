2bd04dfb3e02a5e00f2f41d99ce0045384ab3051
francis.a <francisveru@gmail.com>
2018-04-03 17:54:45 +0530

    add googlemap

--- app/src/main/java/com/grminc/autonomous/HomePage.java
+++ app/src/main/java/com/grminc/autonomous/HomePage.java
@@ -3,11 +3,17 @@ package com.grminc.autonomous;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.anastr.speedviewlib.SpeedView;
+
 public class HomePage extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        SpeedView speedometer = findViewById(R.id.speedView);
+        speedometer.speedTo(50, 4000);
     }
 }
